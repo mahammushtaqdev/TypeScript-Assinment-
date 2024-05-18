7f7002489ee7d9cb000c8d5576eb432685e7784e
Maham <163771631+mahammushtaqdev@users.noreply.github.com>
2024-05-18 22:07:05 +0500

    Add files via upload

---
+++ question1.js
@@ -0,0 +1,45 @@
+"use strict";
+//Problem1
+console.log("Hello, World!");
+//Problem 2
+let temprature = 34;
+if (temprature < 20) {
+    console.log("It's Cold!");
+}
+//Problem 3
+let apples = 10;
+let result = apples - 3;
+console.log(result);
+//Problem 4
+let firstName = "Maham";
+let lastName = "Mushtaq";
+let fullName;
+fullName = `${firstName}  ${lastName}`;
+console.log(fullName);
+//Problem 5
+let num = 5;
+if (num > 3) {
+    console.log("yes");
+}
+//Problem 6
+function calculateArea(input) {
+    let area = console.log(`Area of circle radius ${input} is :\t`, 3.14 * (input ** 2));
+    return area;
+}
+calculateArea(2);
+//Problem 7
+for (let i = 1; i <= 50; i++) {
+    if (i % 3 == 0) {
+        console.log(i, "buzz");
+    }
+    else if (i % 5 == 0) {
+        console.log(i, "fizz");
+    }
+    else {
+        console.log(i, "fizz");
+    }
+    {
+        console.log(i);
+    }
+}
+//Problem 8
